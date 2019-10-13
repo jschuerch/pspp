c2abd35ca5b9263630749005c060d7898a8624e1
Jennifer Schürch <jennifermschuerch@gmail.com>
2019-10-13 14:38:27 +0200

    Add if-else and while to Program

--- P04_Compiler_P3/src/Program.java
+++ P04_Compiler_P3/src/Program.java
@@ -1,3 +1,4 @@
+import com.sun.jdi.Value;
 import de.inetsoftware.jwebassembly.JWebAssembly;
 import de.inetsoftware.jwebassembly.module.*;
 
@@ -5,22 +6,35 @@ public class Program implements Emitter {
 
     static void program() throws Exception {
         statementSequence();
-        /*JWebAssembly.il.add(new WasmLoadStoreInstruction(true, JWebAssembly.local(ValueType.f64, "value"), 0));
-        JWebAssembly.il.add(new WasmBlockInstruction(WasmBlockOperator.RETURN, null, 0));*/
+        JWebAssembly.il.add(new WasmBlockInstruction(WasmBlockOperator.UNREACHABLE, 0));
     }
 
     static void statementSequence() throws Exception {
-        statement();
         do {
             statement();
-        } while (Scanner.la != Token.EOF);
+        } while (Scanner.la != Token.EOF && Scanner.la != Token.RCBRACK);
     }
 
     static void statement() throws Exception {
-        if (Scanner.la == Token.IDENT)
-            assignment();
-        else if (Scanner.la == Token.RETURN)
-            returnStatement();
+        switch (Scanner.la) {
+            case Token.IDENT:
+                assignment();
+                break;
+            case Token.RETURN:
+                returnStatement();
+                break;
+            case Token.IF:
+                ifStatement();
+                break;
+            case Token.WHILE:
+                whileStatement();
+                break;
+            case Token.LCBRACK:
+                block();
+                break;
+            default:
+                System.out.println("bla");
+        }
     }
 
     static void assignment() throws Exception {
@@ -38,32 +52,42 @@ public class Program implements Emitter {
         Scanner.check(Token.SCOLON);
         JWebAssembly.il.add(new WasmBlockInstruction(WasmBlockOperator.RETURN, null, 0));
     }
-/*
+
     static void ifStatement() throws Exception {
         Scanner.check(Token.IF);
         condition();
-        // TODO Branch to places?
+        JWebAssembly.il.add(new WasmBlockInstruction(WasmBlockOperator.IF, 0));
+
         statement();
         if (Scanner.la == Token.ELSE) {
-            Scanner.scan();
+            Scanner.check(Token.ELSE);
+            JWebAssembly.il.add(new WasmBlockInstruction(WasmBlockOperator.ELSE, 0));
             statement();
         }
-
+        JWebAssembly.il.add(new WasmBlockInstruction(WasmBlockOperator.END, 0));
     }
 
     static void condition() throws Exception {
         Scanner.check(Token.LBRACK);
+
+        boolean isNot = false;
         if (Scanner.la == Token.NOT) {
-            // TODO not...
+            Scanner.check(Token.NOT);
+            isNot = true;
         }
         Calculator.expr();
-        Scanner.check(Token.RBRACK);
-    }
+        JWebAssembly.il.add(new WasmNumericInstruction(NumericOperator.nearest, ValueType.f64, 0));
+        JWebAssembly.il.add(new WasmConvertInstruction(ValueTypeConvertion.d2i, 0));
 
-    static void whileStatement() throws Exception {
-        Scanner.check(Token.WHILE);
-        condition();
-        statement();
+        if (isNot) {
+            JWebAssembly.il.add(new WasmNumericInstruction(NumericOperator.eqz, ValueType.i32,0));
+        } else {
+            JWebAssembly.il.add(new WasmConstInstruction(0.0, 0));
+            JWebAssembly.il.add(new WasmConvertInstruction(ValueTypeConvertion.d2i, 0));
+            JWebAssembly.il.add(new WasmNumericInstruction(NumericOperator.ne, ValueType.i32, 0));
+        }
+
+        Scanner.check(Token.RBRACK);
     }
 
     static void block() throws Exception {
@@ -71,15 +95,63 @@ public class Program implements Emitter {
         statementSequence();
         Scanner.check(Token.RCBRACK);
     }
-*/
+
+    static void whileStatement() throws Exception {
+        Scanner.check(Token.WHILE);
+        JWebAssembly.il.add(new WasmBlockInstruction(WasmBlockOperator.BLOCK, 0));
+        JWebAssembly.il.add(new WasmBlockInstruction(WasmBlockOperator.LOOP,0));
+        condition();
+        JWebAssembly.il.add(new WasmNumericInstruction(NumericOperator.eqz, ValueType.i32,0));
+        JWebAssembly.il.add(new WasmBlockInstruction(WasmBlockOperator.BR_IF, 1,0));
+        statement();
+        JWebAssembly.il.add(new WasmBlockInstruction(WasmBlockOperator.BR,0,0));
+        JWebAssembly.il.add(new WasmBlockInstruction(WasmBlockOperator.END,0));
+        JWebAssembly.il.add(new WasmBlockInstruction(WasmBlockOperator.END,0));
+    }
+
     public static void main(String[] args) throws Exception {
         String bspCode = "x = $arg0;" +
-                "a = 5;" +
+                "a = 1;" +
                 "b = 2 + a;" +
                 "c = 3 + b;" +
+                "if (x) {c = 3;}" +
                 "value = a*x*x + b*x + c;" +
                 "return value;";
-        Scanner.init(bspCode);
+        String test1 = "m = $arg0 - 42;" +
+                "if (m) {" +
+                " a = 7.0 ;" +
+                "} else {" +
+                "a = 5 ;" +
+                "}" +
+                "m = m - a;" +
+                "return m;";
+        String test2 = "m = $arg0 - 42;" +
+                "if (!m) {" +
+                "return 7;" +
+                "} else {" +
+                "return 666;" +
+                "}";
+        String test3 = "m = $arg0;" +
+                "s = 1;" +
+                "while(m){" +
+                "s = s + 1;" +
+                "m = m-1;" +
+                "}" +
+                "return s;";
+        String test4 = "m = $arg0;" +
+                "s = 1;" +
+                "while(m){" +
+                "s = s * m;" +
+                "m = m-1;" +
+                "}" +
+                "return s;";
+        /*String bspCode = "m = $arg0 - 42;\n" +
+                "if (!m) {\n" +
+                "return 7;\n" +
+                "} else {\n" +
+                "return 666;\n" +
+                "}";*/
+        Scanner.init(test3);
         Scanner.scan();
         JWebAssembly.emitCode(IProgram.class, new Program());
     }
